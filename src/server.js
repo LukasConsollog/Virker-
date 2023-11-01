1e853318bb1134660c7e3dacb99cb4df4a37c5a1
LukasConsollog <lupe22ae@student.cbs.dk>
2023-11-01 21:30:56 +0100

    home.html

--- src/server.js
+++ src/server.js
@@ -3,20 +3,34 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = 5500;
 app.use(cors());
 app.use(bodyParser.json());
-
+const path = require("path");
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "client")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "pages", "home.html"));
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, this is your server!");
+});
+app.use(express.static(path.join(__dirname, "Client")));
+
+// Endpoint to handle review submission
 const juices = [
   // ... (your juice objects)
 ];
-
-// Endpoint to handle review submission
 app.post("/submit-review", (req, res) => {
   const { username, juiceName, stars, review } = req.body;
-
+  console.log("Received review submission:", {
+    username,
+    juiceName,
+    stars,
+    review,
+  });
   // Find the corresponding juice by name
   const juice = juices.find((juice) => juice.name === juiceName);
 
