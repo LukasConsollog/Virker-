b9fcd3224a153d5f9d7e1444ea8fb737bf1e9c70
LukasConsollog <lupe22ae@student.cbs.dk>
2023-11-01 20:47:02 +0100

    Fuld Omskrivning

--- src/server.js
+++ src/server.js
@@ -1,26 +1,39 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
-const path = require("path");
 
 const app = express();
-
-const customerRoute = require("./routes/customer");
-const cloudinaryRoute = require("./routes/cloudinary");
-
+const port = 5500;
 app.use(cors());
+app.use(bodyParser.json());
+
 app.use(express.json());
 
-app.use("/customer", customerRoute);
-app.use("/cloudinary", cloudinaryRoute);
+const juices = [
+  // ... (your juice objects)
+];
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+// Endpoint to handle review submission
+app.post("/submit-review", (req, res) => {
+  const { username, juiceName, stars, review } = req.body;
+
+  // Find the corresponding juice by name
+  const juice = juices.find((juice) => juice.name === juiceName);
+
+  if (!juice) {
+    return res.status(404).json({ error: "Juice not found" });
+  }
+
+  // Add the review to the juice object
+  juice.reviews.push({
+    username,
+    stars,
+    review,
+  });
+
+  res.json({ message: "Review submitted successfully" });
 });
 
-app.get("/home", (req, res) => {
-  res.sendFile(path.join(__dirname, "../home.html"));
-});
-
-app.listen(3000, () => {
-  console.log("Server open on port 3000");
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
